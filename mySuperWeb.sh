408c1957523e2643e0acae85d884f4384476adbd
Deras <sergio deras@tiempodevelopment.com>
2020-06-15 17:22:43 -0500

    Update URL pattern

--- mySuperWeb.sh
+++ mySuperWeb.sh
@@ -2,4 +2,4 @@
 # sudo killall -9 dotnet
 sudo killall -9 myWebApp
 sudo chmod +x /home/ec2-user/mySuperWeb/build_output/myWebApp 
-/home/ec2-user/mySuperWeb/build_output/myWebApp &
+/home/ec2-user/mySuperWeb/build_output/myWebApp &>/dev/null &
