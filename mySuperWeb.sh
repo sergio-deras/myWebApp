25388dac3a66c6cd7a7de2e5b259e8a4fc17fad1
Deras <sergio deras@tiempodevelopment.com>
2020-06-16 17:35:35 -0500

    Update init script to use a diff root path

--- mySuperWeb.sh
+++ mySuperWeb.sh
@@ -1,5 +1,6 @@
 
 # sudo killall -9 dotnet
 sudo killall -9 myWebApp
-sudo chmod +x /home/ec2-user/mySuperWeb/build_output/myWebApp 
-/home/ec2-user/mySuperWeb/build_output/myWebApp &>/dev/null &
+sudo chmod +x /home/ec2-user/mySuperWeb/build_output/myWebApp
+cd /home/ec2-user/mySuperWeb/build_output/
+./myWebApp &
